17828d3e74a1e2588e797dd51eabe6b9fbdfc936
baha-droid <barhan063@gmail.com>
2025-12-14 10:18:13 +0600

    Delete base directory

--- base/tourism/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tour
-
-@admin.register(Tour)
-class TourAdmin(admin.ModelAdmin):
-    list_display = ('title', 'location', 'start_date', 'end_date', 'price')
